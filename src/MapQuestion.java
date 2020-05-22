adb698ee203d4e6161ed8e7c2dc8c14d9e859923
tugba <ttorun16@gmail.com>
2020-05-22 19:38:47 -0400

    New Project is created

---
+++ src/MapQuestion.java
@@ -0,0 +1,12 @@
+public class MapQuestion {
+    public static void main(String[] args) {
+        //	    Modify and print the given map as follows:
+//	    for this problem the map may or may not contain the "a" and "b" keys.
+//	    If both keys are present, append their 2 string values together
+//	    and store the result under the key "ab" and print the value of the "ab" key
+        // if the map doesn't contain both "a" and "b" keys, just print the map
+//	    {"a": "Hi", "b": "There"} → {"a": "Hi", "ab": "HiThere", "b": "There"}
+//	    {"a": "Hi"} → {"a": "Hi"}
+//	    {"b": "There"} → {"b": "There"}
+    }
+}
