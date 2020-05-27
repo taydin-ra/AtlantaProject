c1494ce2dc3131d7b0dbfeaf3ceae8ce922dc0f3
taydin-ra <ttorun16@gmail.com>
2020-05-27 15:08:58 -0400

    back to original

--- src/AnagramQuestion.java
+++ src/AnagramQuestion.java
@@ -23,10 +23,9 @@ public class AnagramQuestion {
         	// Hello
         // hi
 
-        String s = "anagram", t = "nagarammmmm";
+        String s = "anagram", t = "nagaram";
         System.out.println(checkAnagram1(s, t) );
         System.out.println(checkAnagram1("rat1", "car") );
-        System.out.println(checkAnagram1("Trial","liii"));
         System.out.println(checkAnagram1("fatih1", "faith") );
         System.out.println(checkAnagram1("fatih", "taf") );
         System.out.println(checkAnagram1("fatih", "tif") );
